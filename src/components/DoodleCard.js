b0997eaaab85b072a92984ec0465e7ac9e1bcaab
aleksarad <alexa.radevich1@gmail.com>
2020-09-02 19:07:04 -0400

    delete popover added

--- src/components/DoodleCard.js
+++ src/components/DoodleCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 import CanvasDraw from "react-canvas-draw";
+import { Popover, OverlayTrigger } from 'react-bootstrap/'
 
 
 
@@ -41,11 +42,24 @@ class DoodleCard extends Component {
   }
 
   renderButtons = () => {
+    const popover = (
+      <Popover id="popover-basic">
+        <Popover.Content>
+          <button className="delete-button" onClick={() => this.props.handleDelete(this.props.doodle.id)}>
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                  </svg>
+          </button>
+        </Popover.Content>
+      </Popover>
+    )
+
     if (this.props.match && this.props.match.url === "/profile") {
      return <span className="edit-delete"> 
+      <OverlayTrigger trigger="click" placement="left" overlay={popover}>
+        <button className="button">delete</button> 
+      </OverlayTrigger>
       <button className="button" data-toggle="modal" data-target="#canvasModal" onClick={this.handleEdit}>edit</button>
-      <button className="button" onClick={() => this.props.handleDelete(this.props.doodle.id)}>delete</button> 
-      {/* <button className="button" >download</button>  */}
       <svg onClick={this.handleClick} width="1.3em" height="1.3em" viewBox="0 0 16 16" className="align-bottom bi bi-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M.5 8a.5.5 0 0 1 .5.5V12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8.5A.5.5 0 0 1 .5 8z"/>
         <path fill-rule="evenodd" d="M5 7.5a.5.5 0 0 1 .707 0L8 9.793 10.293 7.5a.5.5 0 1 1 .707.707l-2.646 2.647a.5.5 0 0 1-.708 0L5 8.207A.5.5 0 0 1 5 7.5z"/>
@@ -65,7 +79,20 @@ class DoodleCard extends Component {
   }
 
   render() {
-    console.log(this.props)
+
+        
+    // const popover = (
+    //   <Popover id="popover-basic">
+    //     <Popover.Content>
+    //       <button className="delete-button" onClick={() => this.props.handleDelete(this.props.doodle.id)}>
+    //               <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    //                   <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+    //               </svg>
+    //       </button>
+    //     </Popover.Content>
+    //   </Popover>
+    // )
+   
     const doodle = this.props.doodle
     const doodleData = JSON.stringify(doodle.doodle_data)
 
