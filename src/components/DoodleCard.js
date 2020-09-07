7f4b82b20c906b1cd1a57e4151059577a73b86c6
aleksarad <alexa.radevich1@gmail.com>
2020-09-07 19:11:45 -0400

    reverted like disabling for now

--- src/components/DoodleCard.js
+++ src/components/DoodleCard.js
@@ -130,7 +130,7 @@ class DoodleCard extends Component {
             </div>
             <div className="like">
               <span>{doodle.likes.length} </span>
-              <svg width="1em" height="1em" viewBox="0 0 16 16" onClick={this.props.user.id ? this.handleLike : null} className={this.state.likeStatus ? "like align-middle bi bi-heart-fill" : "liked align-middle bi bi-heart-fill"} xmlns="http://www.w3.org/2000/svg">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" onClick={this.handleLike} className={this.state.likeStatus ? "like align-middle bi bi-heart-fill" : "liked align-middle bi bi-heart-fill"} xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
               </svg>
             </div>
